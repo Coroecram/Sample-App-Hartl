fc4c714d1b4c6c713ca4552334a463ff7d2faa7f
Coroecram <michael.budnick@outlook.com>
2015-09-13 00:38:19 +0000

    Chapter 9 Exercises

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+  
   test "layout links" do
+  
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -9,5 +14,23 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
+  end
+  
+  test "login layout links" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", users_path
+    #assert_select dropdown menu?
+    #toggle dropdown
+    #assert_select "a[href=?]", current_user
+    #assert_select "a[href=?]", edit_user_path
+    assert_select "a[href=?]", logout_path
   end
 end
