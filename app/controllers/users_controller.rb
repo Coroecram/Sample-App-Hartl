4ec46aa55e2104725b4c1222d58a3e4b90e79e2f
Coroecram <michael.budnick@outlook.com>
2015-09-16 00:16:52 +0000

    Add user microposts chapter 11 accomplished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,18 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
-  def index
-    @users = User.paginate(page: params[:page])
-  end
-
   def new
     @user = User.new
   end
   
-  def show 
+  def index
+    @users = User.where(activated: true).paginate(page: params[:page])
+  end
+
+  def show
     @user = User.find(params[:id])
-    #debugger
+    redirect_to root_url and return unless !@user.nil?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def create
@@ -52,15 +53,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                 :password_confirmation)
-  end
-  
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
   end
   
   # Confirms the correct user.
